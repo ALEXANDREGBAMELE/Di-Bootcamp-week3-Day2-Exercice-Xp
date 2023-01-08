6eff913b7eaad562b1631069e451e96230308683
ALEXANDREGBAMELE <112797022+ALEXANDREGBAMELE@users.noreply.github.com>
2023-01-08 02:50:46 +0100

    adding Exercice 2

---
+++ Exercice2/script2.js
@@ -0,0 +1,42 @@
+        //Retrieve the form and console.log it.
+        let form = document.forms[0]
+
+        //Retrieve the inputs by their id and console.log them.
+        console.log(form)
+        let fname = document.getElementById("fname")
+        console.log(fname)
+        let lname = document.getElementById("lname")
+        console.log(lname)
+        let submit = document.getElementById("submit")
+        console.log(submit)
+       //Retrieve the inputs by their name attribute and console.log them.
+
+        let fnam = form.elements.fname
+        console.log(fnam)
+        let lnam = form.elements.lname
+        console.log(lnam)
+        let submi = form.elements.submit
+        console.log(submi)
+
+        submi.addEventListener("click" , function (e) {
+            e.preventDefault()
+            submi.style.backgroundColor = "lightgreen"
+        })
+        
+        let ul = document.querySelector("ul")
+        let li = document.createElement("li")
+        console.log(ul.appendChild(li))
+
+
+// When the user submits the form (ie. submit event listener)
+// use event.preventDefault(), why ?
+// get the values of the input tags,
+// make sure that they are not empty,
+// create an li per input value,
+// then append them to a the <ul class="usersAnswer"></ul>, below the form.
+// The output should be :
+
+// <ul class="usersAnswer">
+//     <li>first name of the user</li>
+//     <li>last name of the user</li>
+// </ul>
---
+++ Exercice3/exercice3.js
@@ -0,0 +1,53 @@
+// Add this sentence to your HTML file then follow the steps :
+
+// <p><strong>Hello</strong> I hope you are enjoying <strong>this</strong> class. At the
+// <strong>end</strong> you <strong>will</strong> be great Developers!
+// <strong>Enjoy</strong> the <strong>JavaScript </strong> lessons</p>
+
+
+// In the JS file:
+
+// Declare a global variable named allBoldItems.
+
+// Create a function called getBold_items() that takes no parameter. This function should collect all the bold items inside the paragraph and assign them to the allBoldItems variable.
+
+// Create a function called highlight() that changes the color of all the bold text to blue.
+
+// Create a function called return_normal() that changes the color of all the bold text back to black.
+
+// Call the function highlight() on mouseover (ie. when the mouse pointer is moved onto the paragraph), and the function return_normal() on mouseout (ie. when the mouse pointer is moved out of the paragraph). Look at this example
+
+
+// 🌟 Exercice 4 : Volume Of A Sphere
+// Instructions
+// Write a JavaScript program to calculate the volume of a sphere. Use the code below as a base:
+// <!doctype html> 
+// <html lang="en"> 
+//     <head> 
+//         <meta charset="utf-8"> 
+//         <title>Volume of a Sphere</title> 
+//         <style>  
+//             body {
+//                 padding-top:30px;
+//             } 
+
+//             label,input {
+//                 display:block;
+//             }  
+//         </style> 
+//     </head> 
+//     <body> 
+//         <p>Input radius value and get the volume of a sphere.</p> 
+//         <form  id="MyForm"> 
+//             <label for="radius">Radius</label><input type="text" name="radius" id="radius" required> 
+//             <label for="volume">Volume</label><input type="text" name="volume" id="volume"> 
+//             <input type="submit" value="Calculate" id="submit">    
+//         </form> 
+//     </body> 
+// </html> 
+
+
+// Exercise 5 : Event Listeners
+// Instructions
+// Add many events listeners to one element on your webpage. Use the click, mouseover, mouseout and dblclick events.
+// Each listener should do a different thing, for instance - change position x, change position y, change color, change the font size… and more.
